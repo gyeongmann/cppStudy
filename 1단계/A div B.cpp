1e936e7f7f82aa73301925b0b6ae19b7a7dc0cc8
gyeongmann <128693089+gyeongmann@users.noreply.github.com>
2024-01-07 20:56:28 +0900

    Create A div B.cpp

---
+++ 1단계/A div B.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a;
+    
+    cin >> a;
+    
+    double b;
+    
+    cin >> b;
+    
+    cout.precision(10);
+    cout << a/b;
+    return 0;
+}
